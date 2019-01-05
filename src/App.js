88e75a9286b9b52d40b5731a208dc27a694baf1b
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-01-05 18:13:36 -0500

    Made the search bar & title stay in place w/ css helper classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import{ robots } from './robots';
 import './App.css';
 
 
@@ -10,9 +9,16 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
          }
+     }
+
+
+     componentDidMount(){
+         fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> (response.json())
+            .then(users => this.setState({robots: users})));
      }
     
     onSearchChange = (event) => {
@@ -25,14 +31,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-
+        if (this.state.robots.length === 0) {
+            return <h1>loading</h1>
+        } else {
         return(
             <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox  searchChange={this.onSearchChange}/>
+                <div className='sticky'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox  searchChange={this.onSearchChange}/>
+                </div>
                 <CardList  robots={filteredRobots}/>
             </div>
-        );
+        );}
     }
 }
 
